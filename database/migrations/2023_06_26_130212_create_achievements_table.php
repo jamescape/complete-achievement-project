a62fe4af106d0bf59fdca01d227c5e93ab3e5428
Tom Houghton-Alliss <tom@harkerapp.com>
2026-04-16 10:26:02 +0100

    Added achievement name, id, and description to migration

--- database/migrations/2023_06_26_130212_create_achievements_table.php
+++ database/migrations/2023_06_26_130212_create_achievements_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('achievements', function (Blueprint $table) {
             $table->id();
+            $table->string('achievement_name');
+            $table->string('achievement_description');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
