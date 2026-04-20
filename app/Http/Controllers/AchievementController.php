1ec3f60fd20ea4a6bff5fc91aa1646a44b19e615
Tom Houghton-Alliss <tom@harkerapp.com>
2026-04-20 10:06:59 +0100

    Added CRUD for Achievements

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -7,5 +7,38 @@ use App\Models\Achievement;
 
 class AchievementController extends Controller
 {
-    //
+    public function index()
+    {
+        return Achievement::all();
+    }
+
+    public function show(string $id)
+    {
+        $achievement = Achievement::findOrFail($id);
+
+        return response()->json($achievement, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $user = Achievement::create($request->all());
+
+        return response()->json($user, 201);
+    }
+
+    public function delete($id)
+    {
+        $user = Achievement::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'Deleted sucessfully.'], 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $user = Achievement::findOrFail($id);
+        $user->upadate($request->all());
+
+        return response()->json($user, 200);
+    }
 }
